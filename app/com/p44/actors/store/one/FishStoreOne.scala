8cc6d55fe98a1cf2e1699f6744b56a33fe27b7b2
mwilson <mwilson@processor44.com>
2014-01-24 13:00:15 -0700

    Store actors migrated form hodgepodge

---
+++ app/com/p44/actors/store/one/FishStoreOne.scala
@@ -0,0 +1,121 @@
+package com.p44.actors.store.one
+
+import com.p44.models.Fish
+
+import akka.actor.Actor
+import akka.actor.ActorLogging
+import akka.actor.Props
+import akka.actor.actorRef2Scala
+
+/**
+ * The fish store.
+ * Receiving fish, unload, stack, sell, package.
+ *
+ * Delivery of Fish --> Controller --> Unloader --> Catcher + Stacker
+ *
+ * This example uses the Play default thread pool with default settings.
+ */
+object FishStoreOne {
+
+  val UnknownMessage = "unknown"
+
+  val propsController = Props[FishStoreController] // Props is an immutable config object for creating actors
+  val propsUnloader = Props[FishUnloader]
+  val propsCatcher = Props[FishCatcher]
+  val propsStacker = Props[FishStacker]
+
+  // MESSAGES
+  case object Echo
+  case object Done
+  case class Deliver(shipment: List[Fish])
+  case class Unload(fish: Fish) // pass to catcher
+  case class Catch(fish: Fish) // catch and hand off to an available stacker
+  case class Stack(fish: Fish) // put on ice
+
+}
+
+/** 
+ *  Controller 
+ *  
+ *  controller ! FishStoreOne.Deliver(shipment) --> Unloader --> Catcher + Stacker
+ *  
+ *  "like people" (that appear and disappear)
+ */
+class FishStoreController extends Actor with ActorLogging {
+  // receiving messages is what an actor does
+  def receive = { // PartialFunction[Any, Unit]  
+    case FishStoreOne.Deliver(shipment) => {
+      log.info("New delivery of this many fish: " + shipment.size)
+      // create one new unloader for each delivery
+      val unloader = context.actorOf(FishStoreOne.propsUnloader)
+      shipment.foreach { x => unloader ! FishStoreOne.Unload(x) }
+    }
+    case FishStoreOne.Done => print("u")
+    case FishStoreOne.Echo => sender ! "Echo"
+    case _ => sender ! FishStoreOne.UnknownMessage
+  }
+}
+
+/** Unload */
+class FishUnloader extends Actor with ActorLogging {
+  def receive = {
+    case FishStoreOne.Unload(fish) => {
+      log.debug("Unloaded " + fish)
+      val catcher = context.actorOf(FishStoreOne.propsCatcher) // creates one new catcher per fish
+      catcher ! FishStoreOne.Catch(fish) // catcher will stop itself
+      context.parent ! FishStoreOne.Done
+    }
+    case FishStoreOne.Done => print("c")
+    case _ => log.error("unknown case")
+  }
+  def stop() = {
+    context.parent ! FishStoreOne.Done
+    context.stop(self)
+  }
+}
+
+/** Catch */
+class FishCatcher extends Actor with ActorLogging {
+  def receive = {
+    case FishStoreOne.Catch(fish) => {
+      log.debug("Catch " + fish)
+      val stacker = context.actorOf(FishStoreOne.propsStacker) // create new stacker
+      stacker ! FishStoreOne.Stack(fish)
+    }
+    case FishStoreOne.Done => {
+      print("s")
+      stop()
+    }
+    case _ => log.error("unknown case")
+  }
+  def stop() = {
+    context.parent ! FishStoreOne.Done
+    context.stop(self)
+  }
+}
+
+/** Stack */
+class FishStacker extends Actor with ActorLogging {
+  def receive = {
+    case FishStoreOne.Stack(fish) => {
+      log.debug("Stack " + fish)
+      packOnIce(fish)
+      context.parent ! FishStoreOne.Done
+    }
+    case _ => log.error("unknown case")
+  }
+  def packOnIce(fish: Fish) = {
+    log.info("Packed on Ice: " + fish)
+  }
+  // the parent will stop itself and this child will stop as a consequence 
+  // if stacker stops itself here we get...
+  // Message [akka.dispatch.sysmsg.Terminate] was not delivered. [1] dead letters encountered. 
+}
+
+// ISSUES - This uses actors but doesn't do much of interest
+// 1. FishStoreOne.Deliver - The unloader is created but not stopped and not reused.  It could easily be reused
+// 2. Catch and Stack don't have any significant behavior, results only in a log.info
+// 3. It would be nice to send a delivery summary to the initiator - count and total weight.
+// 4. How do we see the fish catching in the UI?
+
+// go to FishStoreTwo for addressing the issues.
---
+++ app/com/p44/actors/store/two/FishStoreTwo.scala
@@ -0,0 +1,226 @@
+package com.p44.actors.store.two
+
+import com.p44.models._
+
+import akka.actor.Actor
+import akka.actor.ActorLogging
+import akka.actor.Props
+import akka.actor.actorRef2Scala
+import akka.pattern.{ ask, pipe }
+import akka.util.Timeout
+import com.p44.broadcast.FishStoreBroadcaster
+import play.api.libs.json._
+import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{ Failure, Success }
+
+/**
+ * Changes and additions from One:
+ * 1.  Actor reference management
+ * 2.  Catcher now has a chance of drop fish with recovery
+ * 3.  Catcher now demonstrates behavior state change with context.become
+ * 4.  Added a Commentator that broadcasts the dropped fish events.
+ */
+object FishStoreTwo {
+
+  val UnknownMessage = "unknown"
+
+  val propsController = Props[FishStoreController]
+  val propsDeliveryCalculator = Props[FishDeliveryCalculator]
+  val propsUnloader = Props[FishUnloader]
+  val propsCatcher = Props[FishCatcher]
+  val propsStacker = Props[FishStacker]
+  val propsCommentator = Props[FishCommentator]
+
+  val commentatorName = "commentatorTwo"
+
+  // MESSAGES
+  case object Echo
+  case object Done
+  case class Deliver(shipment: List[Fish])
+  case class GenerateReceipt(ts: Long, shipment: List[Fish]) // New functionality, returns DeliveryReceipt
+  case class Unload(ts: Long, fish: Fish)
+  case class Catch(ts: Long, fish: Fish) // catch and hand off to an available stacker
+  case class Stack(ts: Long, fish: Fish) // persist to db
+  case class AnnounceDroppedFish(droppedFish: DroppedFish) // New functionality to watch dropped fish
+
+  lazy val possibleExclamations = Seq("Darn it!", "I thought I had it!", "No no no!", "Sippery one!", "I wasn't looking",
+    "Nuts!", "Aaaaaa!", "Dammit!", "Are you kidding?", "Bad throw.", "Bollocks!", "Nunen", "That's weird", "Again?")
+
+  /** Build the receipt */
+  def calcReceipt(ts: Long, shipment: List[Fish]): DeliveryReceipt = {
+    val totalWeight: Double = shipment.map(x => x.pounds).sum
+    val time = FishStoreModels.formatTimstampMillis(ts, FishStoreModels.DATE_FORMATTER_USA)
+    // DeliveryReceipt(id: Long, fishCount: Int, totalWeight: Double, payment: Double, time: String)
+    DeliveryReceipt(ts, shipment.size, totalWeight, totalWeight * 3.0, time: String, "Thank You!")
+  }
+}
+
+/** Controller */
+class FishStoreController extends Actor with ActorLogging {
+
+  // one dedicated unloader actor (handles all unload messages queued in order)
+  val unloaderRef = context.actorOf(FishStoreTwo.propsUnloader)
+  val calculatorRef = context.actorOf(FishStoreTwo.propsDeliveryCalculator)
+
+  implicit val timeout = Timeout(5.seconds) // used for ask (?)
+
+  def receive = {
+    case FishStoreTwo.Deliver(shipment) => {
+      val now = System.currentTimeMillis
+      log.info("New delivery of this many fish: " + shipment.size)
+      shipment.foreach { x => unloaderRef ! FishStoreTwo.Unload(now, x) }
+      
+      // ask the calculator for a receipt, send the future back to sender
+      val futureRecepit = (calculatorRef ? FishStoreTwo.GenerateReceipt(now, shipment))
+      futureRecepit pipeTo sender // sends a future receipt to sender as Future[Any]
+      // pipeTo slide
+      
+      // An alternate implementation: local calc in the actor
+      //sender ! FishStoreTwo.calcReceipt(now, shipment) 
+    }
+    case FishStoreTwo.Done => print("u")
+    case FishStoreTwo.Echo => sender ! "Echo"
+    case _ => sender ! FishStoreTwo.UnknownMessage
+  }
+
+}
+
+class FishDeliveryCalculator extends Actor with ActorLogging {
+  def receive = {
+    case FishStoreTwo.GenerateReceipt(ts, shipment) => {
+      val dr: DeliveryReceipt = FishStoreTwo.calcReceipt(ts, shipment)
+      log.info("GenerateReceipt: " + dr)
+      sender ! dr // send it back or we timeout
+    }
+    case _ => sender ! FishStoreTwo.UnknownMessage
+  }
+}
+
+// val uloaderSel = context.actorSelection(unloaderRef.path) // reuse the unloader for each delivery
+
+/** Unload */
+class FishUnloader extends Actor with ActorLogging {
+
+  // the commentator broadcasting messages about the workings of the store
+  val commentatorRef = context.actorOf(FishStoreTwo.propsCommentator)
+
+  def receive = {
+    case FishStoreTwo.Unload(ts, fish) => {
+      log.debug("Unloaded " + fish)
+      val catcher = context.actorOf(FishStoreTwo.propsCatcher)
+      catcher ! FishStoreTwo.Catch(ts, fish) // catcher will stop itself
+      context.parent ! FishStoreTwo.Done
+    }
+    case FishStoreTwo.AnnounceDroppedFish(droppedFish) => {
+      commentatorRef ! FishStoreTwo.AnnounceDroppedFish(droppedFish)
+    }
+    case FishStoreTwo.Done => print("c")
+    case _ => log.error("unknown case")
+  }
+}
+
+/** Catch */
+class FishCatcher extends Actor with ActorLogging {
+
+  val stacker = context.actorOf(FishStoreTwo.propsStacker) // create new stacker
+
+  def goodhands: Receive = {
+    case FishStoreTwo.Catch(ts, fish) => {
+      val f: Option[Fish] = grabFish(fish, 0.15) // Chance of dropped fish
+      f.isDefined match {
+        case false => { // dropped the fish
+          context.parent ! FishStoreTwo.AnnounceDroppedFish(DroppedFish(fish, exclaim))
+          context.become(butterfingers) // change to butterfingers state
+          self ! FishStoreTwo.Catch(ts, fish) // pick up dropped fish (chance of drop same as catch)
+        }
+        case _ => { stacker ! FishStoreTwo.Stack(ts, fish) }
+      }
+    }
+    case FishStoreTwo.Done => {
+      print("s")
+      stop()
+    }
+  }
+
+  def butterfingers: Receive = {
+    case FishStoreTwo.Catch(ts, fish) => {
+      val f: Option[Fish] = grabFish(fish, 0.20)
+      f.isDefined match {
+        case false => { // dropped the fish
+          context.parent ! FishStoreTwo.AnnounceDroppedFish(DroppedFish(fish, exclaim))
+          self ! FishStoreTwo.Catch(ts, fish) // pick up dropped fish (chance of drop same as catch)
+        }
+        case _ => {
+          stacker ! FishStoreTwo.Stack(ts, fish)
+          context.become(goodhands) // change to goodhands state
+        }
+      }
+    }
+    case FishStoreTwo.Done => {
+      print("c")
+      stop()
+    }
+  }
+
+  def receive = goodhands
+
+  def stop() = {
+    context.parent ! FishStoreTwo.Done
+    context.stop(self)
+  }
+  /** catch the fish, there is a chance of drop */
+  def grabFish(wigglyFish: Fish, odds: Double): Option[Fish] = {
+    (math.random < odds) match {
+      case true => {
+        log.info("Dropped fish: " + wigglyFish)
+        None
+      }
+      case _ => Some(wigglyFish)
+    }
+  }
+  /** say something when dropping a fish */
+  def exclaim: String = {
+    val size = FishStoreTwo.possibleExclamations.size
+    FishStoreTwo.possibleExclamations(scala.util.Random.nextInt(size))
+  }
+}
+
+/** Stack */
+class FishStacker extends Actor with ActorLogging {
+  def receive = {
+    case FishStoreTwo.Stack(ts, fish) => {
+      log.debug("Stack " + fish)
+      packOnIce(ts, fish)
+      context.parent ! FishStoreTwo.Done
+    }
+  }
+  
+  import com.p44.db._ 
+  
+  def packOnIce(dId: Long, fish: Fish) = {
+    val fd = com.p44.db.store.two.FishDelivered(dId, fish.name, fish.pounds, System.currentTimeMillis)
+    val futureInsert = com.p44.db.store.two.FishDeliveredDbHelper.insertOneAsFuture(fd)
+    futureInsert.onComplete {
+      case Failure(e) => log.error("ERROR inserting deliverd fish: " + e.getMessage)
+      case Success(le) => {
+        le.ok match {
+          case false => log.error("ERROR: Not Packed on Ice: " + fish)
+          case _ => log.info("Packed on Ice: " + fish)
+        }
+      }
+    }
+  }
+}
+
+/** Broadcast dropped fish */
+class FishCommentator extends Actor with ActorLogging {
+  def receive = {
+    case FishStoreTwo.AnnounceDroppedFish(droppedFish) => {
+      val jsv: JsValue = Json.toJson(droppedFish)
+      FishStoreBroadcaster.fishStoreTwoChannel.push(jsv) // dropped fish as json to the channel
+    }
+  }
+}
+
