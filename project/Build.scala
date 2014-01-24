c402c4b32a1570d317cb8ddcbc20ae3876e85947
mwilson <mwilson@processor44.com>
2014-01-24 12:59:20 -0700

    Project and build settings added

---
+++ project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+import play.Project._
+ 
+object ApplicationBuild extends Build {
+ 
+  val appName         = "FishStore"
+  val appVersion      = "1.0"
+ 
+  val appDependencies = Seq(
+      "com.typesafe.akka" %% "akka-testkit" % "2.2.1" % "test",
+      "joda-time" % "joda-time" % "2.3",
+      "org.reactivemongo" %% "reactivemongo" % "0.10.0",
+      cache
+  )
+ 
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    scalaVersion := "2.10.2"
+  )
+ 
+}
