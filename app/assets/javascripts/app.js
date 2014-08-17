4ae8e87fd51f466329ca0a58e14027976c17d56f
mwilson <mwilson@processor44.com>
2014-08-16 19:46:30 -0600

    Added an angularJs module whaleSightings

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,8 +4,10 @@
 angular.module('fishstoreOne', ['fishstoreOne.controllers', 'ngRoute']);
 angular.module('fishstoreTwo', ['fishstoreTwo.controllers', 'ngRoute']);
 angular.module('fishstoreThree', ['fishstoreThree.controllers', 'ngRoute']);
+angular.module('whaleSightings', ['whaleSightings.controllers', 'ngRoute']);
 
 /** services module initialization, allows adding services to module in multiple files */
 angular.module('fishstoreOne.services', []);
 angular.module('fishstoreTwo.services', []);
 angular.module('fishstoreThree.services', []);
+angular.module('whaleSightings.services', []);
